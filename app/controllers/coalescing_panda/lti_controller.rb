8127119ffc7e0153824ab6d25d1fcaae3d185649
Jake Sorce <jake@instructure.com>
2014-01-13 15:38:29 -0700

    fix user nav lti config setting
    refs PS-1011
    
    Change-Id: I5b529c6b0caf69b290eac4599db9003262175239

--- app/controllers/coalescing_panda/lti_controller.rb
+++ app/controllers/coalescing_panda/lti_controller.rb
@@ -11,20 +11,19 @@ module CoalescingPanda
       tc = IMS::LTI::ToolConfig.new(:title => lti_options[:title], :launch_url => (lti_options[:launch_url])|| 'ABC')
       tc.set_ext_param(platform, :domain, request.host)
       tc.set_ext_param(platform, :privacy_level, 'public')
-      if(lti_options.has_key?(:custom_fields))
+      if lti_options.has_key?(:custom_fields)
         tc.set_ext_param(platform, :custom_fields, lti_options[:custom_fields])
-      
         lti_options[:custom_fields].each do |k, v|
-         tc.set_ext_param(platform, k, v)  
-        end  
+          tc.set_ext_param(platform, k, v)
+        end
       end
 
       lti_nav.each do |k, v|
-        tc.set_ext_param(platform, setting_name(k.to_s), ext_params(v) )
+        tc.set_ext_param(platform, setting_name(k.to_s), ext_params(v))
       end
 
       #strip the launch url
-      xml = tc.to_xml.sub( /<blti:launch_url>.*<\/blti:launch_url>/, '') if lti_options[:launch_url].blank?
+      xml = tc.to_xml.sub(/<blti:launch_url>.*<\/blti:launch_url>/, '') if lti_options[:launch_url].blank?
       render :xml => xml
     end
 
@@ -32,12 +31,12 @@ module CoalescingPanda
 
     def setting_name(name)
       tail = ''
-      if(name == 'course' || name == 'account')
-        tail = '_navigation' unless name.include? "_navigation"
+      if %w(course account user).include?(name)
+        tail = '_navigation' unless name.include? '_navigation'
       end
       (name+tail).to_sym
     end
-      
+
     def ext_params(options)
       url = options.delete(:url)
       options[:url] = main_app.send(url+'_url')
