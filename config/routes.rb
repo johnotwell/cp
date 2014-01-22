71dda4d6abe0688d362278078190cc01fbbded5d
Jake Sorce <jake@instructure.com>
2014-01-22 16:48:27 -0700

    move styleguide from fiu to coalescing panda
    also add a launch root for root_to in host apps
    
    also added some default heading and html body css
      (need to styleguide everything still)
    
    Change-Id: I600773853f873a07f2a5147f3b51961ba00fb959

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 CoalescingPanda::Engine.routes.draw do
   get '/oauth2/redirect' => 'oauth2#redirect'
   get '/config' => 'lti#lti_config'
+  get '/launch' => 'lti#launch'
+  get '/styleguide' => 'lti#styleguide'
 end
