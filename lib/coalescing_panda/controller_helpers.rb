b4549eb8f86245619cf7fb8db7b8f0bb5643f6e0
Brandon Broschinsky <brandonbr@instructure.com>
2014-09-15 08:37:56 -0600

    updated to include session fix in module
    Change-Id: Ia3e8a79feb7950dcb16fa3faa69a6a1c57416d76

--- lib/coalescing_panda/controller_helpers.rb
+++ lib/coalescing_panda/controller_helpers.rb
@@ -1,5 +1,6 @@
 module CoalescingPanda
   module ControllerHelpers
+    require 'useragent'
 
     def canvas_oauth2(*roles)
       return if have_session?
@@ -109,6 +110,21 @@ module CoalescingPanda
           :test
         else
           :production
+      end
+    end
+
+    def session_check
+      user_agent = UserAgent.parse(request.user_agent) # Uses useragent gem!
+      if user_agent.browser == 'Safari' # we apply the fix..
+        return if session[:safari_cookie_fixed] # it is already fixed.. continue
+        if params[:safari_cookie_fix].present? # we should be top window and able to set cookies.. so fix the issue :)
+          session[:safari_cookie_fixed] = true
+          redirect_to params[:return_to]
+        else
+          # Redirect the top frame to your server..
+          query = params.to_query
+          render :text => "<script>var referrer = document.referrer; top.window.location='?safari_cookie_fix=true&return_to='.concat(encodeURI(referrer));</script>"
+        end
       end
     end
 
