7887675cd554efa5ae6d6d58cd0f63bf9af772dc
Jake Sorce <jakesorce@gmail.com>
2014-11-25 16:20:25 -0700

    create users from account context
    Change-Id: If2fca5ae2a0c4f84a6a3294b08692d11be7dc499

--- app/models/coalescing_panda/workers/course_miner.rb
+++ app/models/coalescing_panda/workers/course_miner.rb
@@ -68,7 +68,11 @@ class CoalescingPanda::Workers::CourseMiner
       canvas_id_key = "canvas_#{model_key.to_s.singularize}_id"
       values[canvas_id_key] = values["id"]
       values['workflow_state'] = values["enrollment_state"] if values.has_key?('enrollment_state')
-      record = course.send(model_key).where("#{canvas_id_key} = ?", values['id'].to_s).first_or_initialize
+      if model_key == :users
+        record = account.send(model_key).where("#{canvas_id_key} = ?", values['id'].to_s).first_or_initialize
+      else
+        record = course.send(model_key).where("#{canvas_id_key} = ?", values['id'].to_s).first_or_initialize
+      end
       record.coalescing_panda_lti_account_id = account.id if record.respond_to?(:coalescing_panda_lti_account_id)
       record.assign_attributes(standard_attributes(record, values))
       record.sis_id = sis_id(model_key, values) if record.respond_to?(:sis_id)
