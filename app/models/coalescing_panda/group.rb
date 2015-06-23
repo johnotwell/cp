7a483b0870414dede3803c9e13a78e47ede2c28d
Cody Tanner <ctanner@instructure.com>
2015-06-23 12:32:06 -0600

    makes scheme and api_domain join soft
    Change-Id: I277dd3db5626e5f412285a146dec5e249c981822

--- app/models/coalescing_panda/group.rb
+++ app/models/coalescing_panda/group.rb
@@ -3,7 +3,7 @@ module CoalescingPanda
     belongs_to :context, :polymorphic => true
     include SingleTablePolymorphic
 
-    has_many :group_memberships, dependent: :destroy, foreign_key: :coalescing_panda_group_id, class_name: 'CoalescingPanda::GroupMembership', dependent: :destroy
+    has_many :group_memberships, foreign_key: :coalescing_panda_group_id, class_name: 'CoalescingPanda::GroupMembership', dependent: :destroy
     validates :group_category_id, presence: true
     validates :canvas_group_id, presence: true
     validates :coalescing_panda_user_id, presence: true
