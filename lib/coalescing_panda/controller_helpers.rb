75665020cc7784f400f9185415ba67e91ebc1d21
John Otwell <john.otwell@instructure.com>
2020-05-12 15:23:15 -0600

    Delete session_check code
    Change-Id: I2a70b8091f2cc161a606f02e45efb31ce94aa804

--- lib/coalescing_panda/controller_helpers.rb
+++ lib/coalescing_panda/controller_helpers.rb
@@ -114,18 +114,19 @@ module CoalescingPanda
     end
 
     def session_check
-      user_agent = UserAgent.parse(request.user_agent) # Uses useragent gem!
-      if user_agent.browser == 'Safari' # we apply the fix..
-        return if session[:safari_cookie_fixed] # it is already fixed.. continue
-        if params[:safari_cookie_fix].present? # we should be top window and able to set cookies.. so fix the issue :)
-          session[:safari_cookie_fixed] = true
-          redirect_to params[:return_to]
-        else
-          # Redirect the top frame to your server..
-          query = params.to_query
-          render :text => "<script>var referrer = document.referrer; top.window.location='?safari_cookie_fix=true&return_to='.concat(encodeURI(referrer));</script>"
-        end
-      end
+      logger.warn 'session_check called'
+      # user_agent = UserAgent.parse(request.user_agent) # Uses useragent gem!
+      # if user_agent.browser == 'Safari' # we apply the fix..
+      #   return if session[:safari_cookie_fixed] # it is already fixed.. continue
+      #   if params[:safari_cookie_fix].present? # we should be top window and able to set cookies.. so fix the issue :)
+      #     session[:safari_cookie_fixed] = true
+      #     redirect_to params[:return_to]
+      #   else
+      #     # Redirect the top frame to your server..
+      #     query = params.to_query
+      #     render :text => "<script>var referrer = document.referrer; top.window.location='?safari_cookie_fix=true&return_to='.concat(encodeURI(referrer));</script>"
+      #   end
+      # end
     end
 
   end
