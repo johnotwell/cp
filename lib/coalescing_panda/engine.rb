ca13e2fb22133b317173d7ed2e315e728c1d9701
John Otwell <john.otwell@instructure.com>
2020-05-15 16:53:26 -0600

    Made secure_headers more robust
    Change-Id: I26c2f5edf847a6d88cb5153daa4f9d6bf41023d6

--- lib/coalescing_panda/engine.rb
+++ lib/coalescing_panda/engine.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'secure_headers'
 
 module CoalescingPanda
@@ -7,69 +9,81 @@ module CoalescingPanda
 
     initializer :append_migrations do |app|
       unless app.root.to_s.match root.to_s
-        config.paths["db/migrate"].expanded.each do |expanded_path|
-          app.config.paths["db/migrate"] << expanded_path
+        config.paths['db/migrate'].expanded.each do |expanded_path|
+          app.config.paths['db/migrate'] << expanded_path
         end
       end
     end
 
-    initializer 'coalescing_panda.app_controller' do |app|
+    initializer 'coalescing_panda.app_controller' do |_app|
       OAUTH_10_SUPPORT = true
       ActiveSupport.on_load(:action_controller) do
         include CoalescingPanda::ControllerHelpers
       end
     end
 
-    initializer 'cloaescing_panda.route_helper' do |route|
-      ActionDispatch::Routing::Mapper.send :include, CoalescingPanda::RouteHelpers
+    initializer 'cloaescing_panda.route_helper' do |_route|
+      ActionDispatch::Routing::Mapper.include CoalescingPanda::RouteHelpers
     end
 
-    initializer 'coalescing_panda.route_options', :after => :disable_dependency_loading do |app|
+    initializer 'coalescing_panda.route_options', after: :disable_dependency_loading do |_app|
       ActiveSupport.on_load(:action_controller) do
-        #force the routes to load
+        # force the routes to load
         Rails.application.reload_routes!
-        CoalescingPanda::propagate_lti_navigation
+        CoalescingPanda.propagate_lti_navigation
       end
     end
 
-    initializer :secure_headers do |app|
-      connect_src = %w('self')
-      script_src = %w('self')
+    initializer :secure_headers do |_app|
+      connect_src = %w[self]
+      script_src = %w[self]
       if Rails.env.development?
         # Allow webpack-dev-server to work
-        connect_src << "http://localhost:3035"
-        connect_src << "ws://localhost:3035"
+        connect_src << 'http://localhost:3035'
+        connect_src << 'ws://localhost:3035'
         # Allow stuff like rack-mini-profiler to work in development:
         # https://github.com/MiniProfiler/rack-mini-profiler/issues/327
         # DON'T ENABLE THIS FOR PRODUCTION!
         script_src << "'unsafe-eval'"
       end
-
-      # SecureHeaders::Configuration.default do |config|
-      #   # The default cookie headers aren't compatible with PandaPal cookies currently
-      #   config.cookies = { samesite: { none: true } }
-      #   if Rails.env.production?
-      #     config.cookies[:secure] = true
-      #   end
-      #   # Need to allow LTI iframes
-      #   config.x_frame_options = "ALLOWALL"
-      #   config.x_content_type_options = "nosniff"
-      #   config.x_xss_protection = "1; mode=block"
-      #   config.referrer_policy = %w(origin-when-cross-origin strict-origin-when-cross-origin)
-      #   config.csp = {
-      #       default_src: %w('self'),
-      #       script_src: script_src,
-      #       # Certain CSS-in-JS libraries inline the CSS, so we need to use unsafe-inline for them
-      #       style_src: %w('self' 'unsafe-inline' blob: https://fonts.googleapis.com),
-      #       font_src: %w('self' data: https://fonts.gstatic.com),
-      #       connect_src: connect_src,
-      #   }
-      # end
+      begin
+        SecureHeaders::Configuration.default do |config|
+          # The default cookie headers aren't compatible with PandaPal cookies currently
+          config.cookies = { samesite: { none: true } }
+          config.cookies[:secure] = true if Rails.env.production?
+          # Need to allow LTI iframes
+          config.x_frame_options = 'ALLOWALL'
+          config.x_content_type_options = 'nosniff'
+          config.x_xss_protection = '1; mode=block'
+          config.referrer_policy = %w[origin-when-cross-origin strict-origin-when-cross-origin]
+          config.csp = {
+            default_src: %w[self],
+            script_src: script_src,
+            # Certain CSS-in-JS libraries inline the CSS, so we need to use unsafe-inline for them
+            style_src: %w[self unsafe-inline blob: https://fonts.googleapis.com],
+            font_src: %w[self data: https://fonts.gstatic.com],
+            connect_src: connect_src
+          }
+        end
+      rescue SecureHeaders::Configuration::AlreadyConfiguredError
+        Rails.logger.warn('Could not set default secure headers configuration when there is one already, continuing with previously defined configuration')
+      end
 
       SecureHeaders::Configuration.override(:safari_override) do |config|
         config.cookies = SecureHeaders::OPT_OUT
+        config.x_frame_options = 'ALLOWALL'
+        config.x_content_type_options = 'nosniff'
+        config.x_xss_protection = '1; mode=block'
+        config.referrer_policy = %w[origin-when-cross-origin strict-origin-when-cross-origin]
+        config.csp = {
+          default_src: %w[self],
+          script_src: script_src,
+          # Certain CSS-in-JS libraries inline the CSS, so we need to use unsafe-inline for them
+          style_src: %w[self unsafe-inline blob: https://fonts.googleapis.com],
+          font_src: %w[self data: https://fonts.gstatic.com],
+          connect_src: connect_src
+        }
       end
-
     end
   end
 end
